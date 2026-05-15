6652c54080a19f4ce9b5124a0c644e17fce7a730
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-15 21:49:16 +0300

    Make IDoctorService.cs

---
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Interfaces/IDoctorService.cs
@@ -0,0 +1,29 @@
+﻿using HospitalManagementSystem.Domain.Models;
+
+namespace HospitalManagementSystem.Application.Interfaces;
+
+public interface IDoctorService
+{
+    Task<Doctor?> GetDoctorAsync(int doctorId);
+    Task<Doctor?> GetDoctorByUserIdAsync(string userId);
+    Task<int> GetDoctorsCountAsync();
+    Task<IEnumerable<Doctor>> GetDoctorsAsync();
+
+
+    Task<int> AddDoctorAsync(Doctor doctor);
+    Task UpdateDoctorAsync(Doctor doctor);
+    Task DeleteDoctorAsync(Doctor doctor);
+
+
+    Task<IEnumerable<Doctor>> GetDoctorsByDepartmentAsync(int departmentId);
+    Task<int> GetDoctorsCountInDepartmentAsync(int departmentId);
+
+
+    Task<Doctor?> GetSupervisorAsync(int doctorId);
+    Task<IEnumerable<Doctor>> GetSubordinatesAsync(int doctorId);
+
+
+    Task<IEnumerable<Doctor>> GetDoctorsWithScheduledAppointmentsAsync(int departmentId);
+    Task<IEnumerable<Doctor>> GetDoctorsWithCanceledAppointmentsAsync(int departmentId);
+    Task<IEnumerable<Doctor>> GetDoctorsWithCompletedAppointmentsAsync(int departmentId);
+}
