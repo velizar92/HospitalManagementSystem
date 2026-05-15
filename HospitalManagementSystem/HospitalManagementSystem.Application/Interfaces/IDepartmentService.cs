eb2af4737a053696dc00d48c8af7050481873267
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-15 21:50:31 +0300

    Make IDepartmentService.cs

---
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Interfaces/IDepartmentService.cs
@@ -0,0 +1,12 @@
+﻿using HospitalManagementSystem.Domain.Models;
+
+namespace HospitalManagementSystem.Application.Interfaces;
+
+public interface IDepartmentService
+{
+    Department GetDepartment(int departmentId);
+    IEnumerable<Department> GetDepartments();
+    int AddDepartment(Department department);
+    void DeleteDepartment(Department department);
+    void UpdateDepartment(Department department);
+}
