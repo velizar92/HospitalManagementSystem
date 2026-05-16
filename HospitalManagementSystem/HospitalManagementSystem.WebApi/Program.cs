16cd8529734e67c496885e2fab8bb362c923f7b8
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-16 13:13:30 +0300

    Register all services in the DI container in WebAPI project.

--- HospitalManagementSystem/HospitalManagementSystem.WebApi/Program.cs
+++ HospitalManagementSystem/HospitalManagementSystem.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using HospitalManagementSystem.Application.Implementations;
+using HospitalManagementSystem.Application.Interfaces;
 using HospitalManagementSystem.Infrastructure;
 using HospitalManagementSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
@@ -16,6 +18,17 @@ builder.Services.AddDbContext<HospitalManagementSystemDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IMedicalRecordService, MedicalRecordService>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+builder.Services.AddScoped<IRoomService, RoomService>();
+builder.Services.AddScoped<IAddmissionService, AddmissionService>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
