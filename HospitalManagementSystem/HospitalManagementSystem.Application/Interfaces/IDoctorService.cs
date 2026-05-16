70aa18867cad674d832f933939b3526d50969cd7
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-16 10:58:24 +0300

    Update IDoctorService to use DTOs instead of database models directly.

--- HospitalManagementSystem/HospitalManagementSystem.Application/Interfaces/IDoctorService.cs
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Interfaces/IDoctorService.cs
@@ -1,29 +1,21 @@
-﻿using HospitalManagementSystem.Domain.Models;
+﻿using HospitalManagementSystem.Application.DTOs.Doctor;
 
 namespace HospitalManagementSystem.Application.Interfaces;
 
 public interface IDoctorService
 {
-    Task<Doctor?> GetDoctorAsync(int doctorId);
-    Task<Doctor?> GetDoctorByUserIdAsync(string userId);
+    Task<DoctorDto?> GetDoctorAsync(int doctorId);
+    Task<DoctorDto?> GetDoctorByUserIdAsync(string userId);
     Task<int> GetDoctorsCountAsync();
-    Task<IEnumerable<Doctor>> GetDoctorsAsync();
-
-
-    Task<int> AddDoctorAsync(Doctor doctor);
-    Task UpdateDoctorAsync(Doctor doctor);
-    Task DeleteDoctorAsync(Doctor doctor);
-
-
-    Task<IEnumerable<Doctor>> GetDoctorsByDepartmentAsync(int departmentId);
+    Task<IEnumerable<DoctorDto>> GetDoctorsAsync();
+    Task<IEnumerable<DoctorDto>> GetDoctorsByDepartmentAsync(int departmentId);
     Task<int> GetDoctorsCountInDepartmentAsync(int departmentId);
-
-
-    Task<Doctor?> GetSupervisorAsync(int doctorId);
-    Task<IEnumerable<Doctor>> GetSubordinatesAsync(int doctorId);
-
-
-    Task<IEnumerable<Doctor>> GetDoctorsWithScheduledAppointmentsAsync(int departmentId);
-    Task<IEnumerable<Doctor>> GetDoctorsWithCanceledAppointmentsAsync(int departmentId);
-    Task<IEnumerable<Doctor>> GetDoctorsWithCompletedAppointmentsAsync(int departmentId);
+    Task<int> AddDoctorAsync(DoctorForCreateDto doctorForCreate);
+    Task UpdateDoctorAsync(DoctorForUpdateDto doctorForUpdate);
+    Task DeleteDoctorAsync(int doctorId);
+    Task<DoctorDto?> GetSupervisorAsync(int doctorId);
+    Task<IEnumerable<DoctorDto>> GetSubordinatesAsync(int doctorId);
+    Task<IEnumerable<DoctorDto>> GetDoctorsWithScheduledAppointmentsAsync(int departmentId);
+    Task<IEnumerable<DoctorDto>> GetDoctorsWithCanceledAppointmentsAsync(int departmentId);
+    Task<IEnumerable<DoctorDto>> GetDoctorsWithCompletedAppointmentsAsync(int departmentId);
 }
