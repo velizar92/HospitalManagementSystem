aa247462a8b85398a21dd58c45bcd8c39b35a54d
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-16 12:27:18 +0300

    Add IPrescriptionService.cs with used DTOs.

---
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Interfaces/IPrescriptionService.cs
@@ -0,0 +1,13 @@
+﻿using HospitalManagementSystem.Application.DTOs.Prescription;
+
+namespace HospitalManagementSystem.Application.Interfaces;
+
+public interface IPrescriptionService
+{
+    Task<IEnumerable<PrescriptionDto>> GetPrescriptionsAsync(int doctorId);
+    Task<IEnumerable<PrescriptionDto>> GetPrescriptionsBeforeDateAsync(int doctorId, DateTime date);
+    Task<IEnumerable<PrescriptionDto>> GetPrescriptionsAfterDateAsync(int doctorId, DateTime date);
+    Task<int> CreatePrescriptionAsync(CreatePrescriptionDto prescription);
+    Task UpdatePrescriptionAsync(UpdatePrescriptionDto prescription);
+    Task DeletePrescriptionAsync(int prescriptionId);
+}
