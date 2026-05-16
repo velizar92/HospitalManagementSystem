35942dbda582c0256b26ce86d3727b878290f06a
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-16 11:14:35 +0300

    Update IAppointmentService.cs with DTOs.

---
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Interfaces/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using HospitalManagementSystem.Application.DTOs.Appointment;
+using HospitalManagementSystem.Domain.Enums;
+
+namespace HospitalManagementSystem.Application.Interfaces;
+
+public interface IAppointmentService
+{
+    IEnumerable<AppointmentDto> GetAppointments(int doctorId, AppointmentStatus status);
+    AppointmentDto GetAppointment(int appointmentId);
+    void CreateAppointment(CreateAppointmentDto createAppointmentDto);
+    void UpdateAppointment(UpdateAppointmentDto updateAppointmentDto);
+    void DeleteAppointment(int appointmentId);
+}
