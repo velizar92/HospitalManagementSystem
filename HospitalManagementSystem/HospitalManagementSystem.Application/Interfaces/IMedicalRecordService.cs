f88779aa530c22d1d8af4fade3e2957491e78f95
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-16 12:33:31 +0300

    Update IMedicalRecordService.cs to use async signatures and DTOs instead of domain models directly.

---
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Interfaces/IMedicalRecordService.cs
@@ -0,0 +1,16 @@
+﻿using HospitalManagementSystem.Application.DTOs.MedicalRecord;
+
+namespace HospitalManagementSystem.Application.Interfaces;
+
+public interface IMedicalRecordService
+{
+    Task<MedicalRecordDto> GetMedicalRecordAsync(int medicalRecordId);
+    Task<IEnumerable<MedicalRecordDto>> GetMedicalRecordsAsync();
+    Task<IEnumerable<MedicalRecordDto>> GetMedicalRecordsByDoctorIdAsync(int doctorId);
+    Task<IEnumerable<MedicalRecordDto>> GetMedicalRecordsByPatientIdAsync(int patientId);
+    Task<IEnumerable<MedicalRecordDto>> GetMedicalRecordsBeforeDateAsync(int doctorId, DateTime date);
+    Task<IEnumerable<MedicalRecordDto>> GetMedicalRecordsAfterDateAsync(int doctorId, DateTime date);
+    Task<int> CreateMedicalRecordAsync(CreateMedicalRecordDto createMedicalRecordDto);
+    Task DeleteMedicalRecordAsync(int medicalRecordId);
+    Task UpdateMedicalRecordAsync(UpdateMedicalRecordDto updateMedicalRecordDto);
+}
