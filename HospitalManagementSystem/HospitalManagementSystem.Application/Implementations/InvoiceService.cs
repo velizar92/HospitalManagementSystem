3ff243b8d45b75bf3c03fc493ccb127dfd74e49d
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-17 13:25:00 +0300

    Implement InvoiceService methods.

--- HospitalManagementSystem/HospitalManagementSystem.Application/Implementations/InvoiceService.cs
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Implementations/InvoiceService.cs
@@ -1,42 +1,161 @@
 ﻿using HospitalManagementSystem.Application.DTOs.Invoice;
 using HospitalManagementSystem.Application.Interfaces;
+using HospitalManagementSystem.Domain.Models;
+using HospitalManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalManagementSystem.Application.Implementations;
 
 public class InvoiceService : IInvoiceService
 {
-    public Task<int> CreateInvoiceAsync(CreateInvoiceDto createInvoiceDto)
+    private readonly HospitalManagementSystemDbContext _dbContext;
+
+    public InvoiceService(HospitalManagementSystemDbContext dbContext)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
+
+    }
+    public async Task<int> CreateInvoiceAsync(CreateInvoiceDto createInvoiceDto)
+    {
+        var invoice = new Invoice
+        {
+            PatientId = createInvoiceDto.PatientId,
+            AppointmentId = createInvoiceDto.AppointmentId,
+            CreatedAt = createInvoiceDto.CreatedAt ?? DateTime.UtcNow,
+            Items = createInvoiceDto.Items.Select(invoiceItem => new InvoiceItem
+            {
+                Description = invoiceItem.Description,
+                Amount = invoiceItem.UnitPrice * invoiceItem.Quantity,
+            }).ToList()
+        };
+
+        await _dbContext.AddAsync(invoice);
+        await _dbContext.SaveChangesAsync();
+
+        return invoice.Id;
     }
 
-    public Task DeleteInvoiceAsync(int invoiceId)
+    public async Task<InvoiceDto?> GetInvoiceAsync(int invoiceId)
     {
-        throw new NotImplementedException();
+        return await _dbContext.Invoices
+            .Where(invoice => invoice.Id == invoiceId)
+            .Select(invoice => new InvoiceDto
+            {
+                Id = invoice.Id,
+                PatientId = invoice.PatientId,
+                CreatedAt = invoice.CreatedAt,
+                TotalAmount = invoice.Items.Sum(item => item.Amount),
+                IsPaid = invoice.IsPaid,
+                AppointmentId = invoice.AppointmentId,
+                Items = invoice.Items.Select(item => new InvoiceItemDto
+                {
+                    Description = item.Description,
+                    UnitPrice = item.Amount,
+                    Quantity = 1
+                }).ToList(),
+                PatientFullName = $"{invoice.Patient.Profile.FirstName} {invoice.Patient.Profile.LastName}"
+            })
+            .FirstOrDefaultAsync();
     }
 
-    public Task<InvoiceDto?> GetInvoiceAsync(int invoiceId)
+    public async Task<IEnumerable<InvoiceDto>> GetInvoicesAsync()
     {
-        throw new NotImplementedException();
+        return await _dbContext.Invoices
+             .Select(invoice => new InvoiceDto
+             {
+                 Id = invoice.Id,
+                 PatientId = invoice.PatientId,
+                 CreatedAt = invoice.CreatedAt,
+                 TotalAmount = invoice.Items.Sum(item => item.Amount),
+                 IsPaid = invoice.IsPaid,
+                 AppointmentId = invoice.AppointmentId,
+                 Items = invoice.Items.Select(item => new InvoiceItemDto
+                 {
+                     Description = item.Description,
+                     UnitPrice = item.Amount,
+                     Quantity = 1
+                 }).ToList(),
+                 PatientFullName = $"{invoice.Patient.Profile.FirstName} {invoice.Patient.Profile.LastName}"
+             })
+             .ToListAsync();
     }
 
-    public Task<IEnumerable<InvoiceDto>> GetInvoicesAsync()
+    public async Task<IEnumerable<InvoiceDto>> GetInvoicesByPatientIdAsync(int patientId)
     {
-        throw new NotImplementedException();
+        return await _dbContext.Invoices
+            .Where(invoice => invoice.PatientId == patientId)
+            .Select(invoice => new InvoiceDto
+            {
+                Id = invoice.Id,
+                PatientId = invoice.PatientId,
+                CreatedAt = invoice.CreatedAt,
+                TotalAmount = invoice.Items.Sum(item => item.Amount),
+                IsPaid = invoice.IsPaid,
+                AppointmentId = invoice.AppointmentId,
+                Items = invoice.Items.Select(item => new InvoiceItemDto
+                {
+                    Description = item.Description,
+                    UnitPrice = item.Amount,
+                    Quantity = 1
+                }).ToList(),
+                PatientFullName = $"{invoice.Patient.Profile.FirstName} {invoice.Patient.Profile.LastName}"
+            })
+            .ToListAsync();
     }
 
-    public Task<IEnumerable<InvoiceDto>> GetInvoicesByPatientIdAsync(int patientId)
+    public async Task<IEnumerable<InvoiceDto>> GetUnpaidInvoicesAsync()
     {
-        throw new NotImplementedException();
+        return await _dbContext.Invoices
+            .Where(invoice => !invoice.IsPaid)
+            .Select(invoice => new InvoiceDto
+            {
+                Id = invoice.Id,
+                PatientId = invoice.PatientId,
+                CreatedAt = invoice.CreatedAt,
+                TotalAmount = invoice.Items.Sum(item => item.Amount),
+                IsPaid = invoice.IsPaid,
+                AppointmentId = invoice.AppointmentId,
+                Items = invoice.Items.Select(item => new InvoiceItemDto
+                {
+                    Description = item.Description,
+                    UnitPrice = item.Amount,
+                    Quantity = 1
+                }).ToList(),
+                PatientFullName = $"{invoice.Patient.Profile.FirstName} {invoice.Patient.Profile.LastName}"
+            })
+            .ToListAsync();
     }
 
-    public Task<IEnumerable<InvoiceDto>> GetUnpaidInvoicesAsync()
+    public async Task UpdateInvoiceAsync(UpdateInvoiceDto updateInvoiceDto)
     {
-        throw new NotImplementedException();
+        var invoice = await _dbContext.Invoices.FindAsync(updateInvoiceDto.Id);
+
+        if (invoice == null)
+        {
+            throw new KeyNotFoundException("Invoice not found");
+        }
+
+        invoice.Id = updateInvoiceDto.Id;
+        invoice.IsPaid = updateInvoiceDto.IsPaid ?? invoice.IsPaid;
+        invoice.Items = updateInvoiceDto.Items.Select(invoiceItem => new InvoiceItem
+        {
+            Description = invoiceItem.Description,
+            Amount = invoiceItem.UnitPrice * invoiceItem.Quantity,
+        }).ToList();
+
+        await _dbContext.SaveChangesAsync();
     }
 
-    public Task UpdateInvoiceAsync(UpdateInvoiceDto updateInvoiceDto)
+    public async Task DeleteInvoiceAsync(int invoiceId)
     {
-        throw new NotImplementedException();
+        var invoice = await _dbContext.Invoices.FindAsync(invoiceId);
+
+        if (invoice == null)
+        {
+            throw new KeyNotFoundException("Invoice not found");
+        }
+
+        _dbContext.Invoices.Remove(invoice);
+        await _dbContext.SaveChangesAsync();
     }
 }
