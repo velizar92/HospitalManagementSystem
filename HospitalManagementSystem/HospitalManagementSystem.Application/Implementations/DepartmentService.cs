0724f4c00a2c896f943d77f91d19c759d0fae46f
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-05-22 20:53:46 +0300

    Implement DepartmentService.

--- HospitalManagementSystem/HospitalManagementSystem.Application/Implementations/DepartmentService.cs
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Implementations/DepartmentService.cs
@@ -1,32 +1,81 @@
 ﻿using HospitalManagementSystem.Application.DTOs.Department;
 using HospitalManagementSystem.Application.Interfaces;
+using HospitalManagementSystem.Domain.Models;
+using HospitalManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalManagementSystem.Application.Implementations;
 
 public class DepartmentService : IDepartmentService
 {
-    public Task<int> AddDepartmentAsync(DepartmentCreateDto departmentCreateDto)
+    private readonly HospitalManagementSystemDbContext _dbContext;
+
+    public DepartmentService(HospitalManagementSystemDbContext dbContext)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
     }
 
-    public Task DeleteDepartmentAsync(int departmentId)
+    public async Task<int> AddDepartmentAsync(DepartmentCreateDto departmentCreateDto)
     {
-        throw new NotImplementedException();
+        var department = new Department
+        {
+            Name = departmentCreateDto.Name,
+            Description = departmentCreateDto.Description
+        };
+
+        _dbContext.Departments.Add(department);
+        await _dbContext.SaveChangesAsync();
+
+        return department.Id;
     }
 
-    public Task<DepartmentDto?> GetDepartmentAsync(int departmentId)
+    public async Task<DepartmentDto?> GetDepartmentAsync(int departmentId)
     {
-        throw new NotImplementedException();
+        return await _dbContext.Departments
+            .Where(d => d.Id == departmentId)
+            .Select(d => new DepartmentDto
+            {
+                Id = d.Id,
+                Name = d.Name,
+                Description = d.Description,
+                DoctorCount = d.Doctors.Count
+            })
+            .FirstOrDefaultAsync();
     }
 
-    public Task<IEnumerable<DepartmentDto>> GetDepartmentsAsync()
+    public async Task<IEnumerable<DepartmentDto>> GetDepartmentsAsync()
     {
-        throw new NotImplementedException();
+        return await _dbContext.Departments
+             .Select(d => new DepartmentDto
+             {
+                 Id = d.Id,
+                 Name = d.Name,
+                 Description = d.Description,
+                 DoctorCount = d.Doctors.Count
+             })
+             .ToListAsync();
     }
 
-    public Task UpdateDepartmentAsync(DepartmentUpdateDto departmentUpdateDto)
+    public async Task UpdateDepartmentAsync(DepartmentUpdateDto departmentUpdateDto)
     {
-        throw new NotImplementedException();
+        var department = await _dbContext.Departments.FindAsync(departmentUpdateDto.Id);
+
+        if(department == null)
+        {
+            throw new KeyNotFoundException("Department not found");
+        }
+
+        department.Name = departmentUpdateDto.Name;
+        department.Description = departmentUpdateDto.Description;
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteDepartmentAsync(int departmentId)
+    {
+        var department = await _dbContext.Departments.FindAsync(departmentId);
+
+        _dbContext.Departments.Remove(department!);
+        await _dbContext.SaveChangesAsync();
     }
 }
