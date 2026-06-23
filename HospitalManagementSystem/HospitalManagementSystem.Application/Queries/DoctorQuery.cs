82deca123bfc04b7453561ee9ad86b58a97abe6b
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2026-06-23 22:33:55 +0300

    Add DoctorQuery.cs to the Application project.

---
+++ HospitalManagementSystem/HospitalManagementSystem.Application/Queries/DoctorQuery.cs
@@ -0,0 +1,9 @@
+﻿using HospitalManagementSystem.Domain.Enums;
+
+namespace HospitalManagementSystem.Application.Queries;
+
+public class DoctorQuery
+{
+    public int? DepartmentId { get; set; }
+    public AppointmentStatus? AppointmentStatus { get; set; }
+}
